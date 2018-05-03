bd456003e17fe131fc0a66b7889dc8de2b63c09f
sivakumar <sivakumar@Shiva>
2018-05-02 20:59:27 -0400

    updated TC1 and TC2

--- Scripts/SampleTC2/Script1525309040496.groovy
+++ Scripts/SampleTC2/Script1525309040496.groovy
@@ -19,7 +19,7 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.openBrowser('www.gmail.com')
+WebUI.openBrowser('www.yahoo.com')
 
 WebUI.closeBrowser()
 
