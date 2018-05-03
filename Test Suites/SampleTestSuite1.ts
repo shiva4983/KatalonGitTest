fcfc3c99ca8fe16aef178cda08885fa6d86d705d
sivakumar <sivakumar@Shiva>
2018-05-02 21:04:07 -0400

    updated test suite and test suite collections

---
+++ Test Suites/SampleTestSuite1.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SampleTestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>v.sivakumaar@gmail.com;</mailRecipient>
+   <numberOfRerun>1</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>true</rerunFailedTestCasesOnly>
+   <testSuiteGuid>29126aa4-1bbd-4469-83da-e5095d60509b</testSuiteGuid>
+   <testCaseLink>
+      <guid>e36eb30a-770e-4f59-9ca8-27b9b7cd498b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SampleTC1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c9ba3117-1830-488f-96e9-46099c529834</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SampleTC2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/SampleTestSuiteCollection.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>SampleTestSuiteCollection</name>
+   <tag></tag>
+   <executionMode>SEQUENTIAL</executionMode>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <runConfigurationId>Firefox</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/SampleTestSuite1</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
